7d9c274d7a5833270637e03e8eabc4a18ec4b904
Jonas Renault <jonasrenault@gmail.com>
2024-11-28 21:15:05 +0100

    set basename in router

--- src/router.tsx
+++ src/router.tsx
@@ -12,4 +12,6 @@ export const routes = [
   },
 ]
 
-export const router = createBrowserRouter(routes)
+export const router = createBrowserRouter(routes, {
+  basename: import.meta.env.BASE_URL,
+})
